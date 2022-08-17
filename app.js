d36f891737d8188842923f9819059dce1dab58e1
Hamedhosseini1999 <hamed.husseini.official1999@gmail.com>
2022-08-17 13:22:49 +0430

    Create-Header_Showcase_Suggestion-components

---
+++ app.js
@@ -0,0 +1,21 @@
+let swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+let SuggesttionsSlider = new Swiper(".mySuggestion", {
+  slidesPerView: 4,
+  initialSlide: 2,
+  centeredSlides: true,
+  spaceBetween: 30,
+  grabCursor: true,
+});
