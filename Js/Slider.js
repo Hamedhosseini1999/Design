cb686eefa61abb0780d306b58a6f3bf0c3bc4a82
Hamedhosseini1999 <hamed.husseini.official1999@gmail.com>
2022-08-23 20:28:58 +0430

    Create-Slider-Box-to-Set-Services

--- Js/Slider.js
+++ Js/Slider.js
@@ -12,6 +12,28 @@ let swiper = new Swiper(".mySwiper", {
   keyboard: true,
 });
 
+// Services
+
+const next = document.querySelector(".right");
+
+let setServices = new Swiper(".service-container", {
+  breakpoints: {
+    350: { slidesPerView: 1 },
+    600: { slidesPerView: 2 },
+    800: { slidesPerView: 3 },
+    1200: { slidesPerView: 4 },
+  },
+  slidesPerView: 4,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loopFillGroupWithBlank: true,
+  loop: true,
+  navigation: {
+    nextEl: ".left",
+    prevEl: ".right",
+  },
+});
+
 let SuggesttionsSlider = new Swiper(".mySuggestion", {
   breakpoints: {
     550: { slidesPerView: 2 },
